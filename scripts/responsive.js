63d4a19c36c49d41c7ead5dddc4b5413befa4b26
AstroPilot2808 <ajayohm28@gmail.com>
2023-03-20 21:45:10 -0400

    Parallax Scrolling and Final Touches

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -21,3 +21,20 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right="-200px";
     }
+// JavaScript for Adding Form Data from User to Google Sheets
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxV0ubfhgcjQAS4WgrLIg_3VJ-URecy_PwbH3vKQjdnYiisL7jSR64dhX4WJWzaVTvm/exec'
+    //              The name here \|/ \|/ \|/ \|/ \|/ \|/must match name attribute of form in index.html
+  const form = document.forms['submit-to-google-sheet']
+  const msg=document.getElementById("msg")
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML=""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
